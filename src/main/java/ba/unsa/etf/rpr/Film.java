d7d575ff8d571e88619846407f37f106a8d80ae5
halilovick <khalilovic02@gmail.com>
2022-12-09 00:04:13 +0100

    film class added

---
+++ src/main/java/ba/unsa/etf/rpr/Film.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rpr;
+
+public class Film {
+    private int id;
+    private String ime;
+    private String zanr;
+    private int trajanje;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getZanr() {
+        return zanr;
+    }
+
+    public void setZanr(String zanr) {
+        this.zanr = zanr;
+    }
+
+    public int getTrajanje() {
+        return trajanje;
+    }
+
+    public void setTrajanje(int trajanje) {
+        this.trajanje = trajanje;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{id=" + id + ", ime='" + ime + "\'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id == film.id;
+    }
+}
