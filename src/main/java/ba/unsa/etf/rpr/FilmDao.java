903afed5a904722916b89bc364167b0fab35690e
halilovick <khalilovic02@gmail.com>
2022-12-09 23:33:30 +0100

    implemented additional features to FilmDao interface

--- src/main/java/ba/unsa/etf/rpr/FilmDao.java
+++ src/main/java/ba/unsa/etf/rpr/FilmDao.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr;
 
+import java.util.List;
+
 public interface FilmDao extends Dao<Film>{
+    List<Film> searchByTrajanje(int a, int b);
+    List<Film> searchByZanr(String zanr);
 }
--- src/main/java/ba/unsa/etf/rpr/FilmDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/FilmDaoSQLImpl.java
@@ -43,12 +43,13 @@ public class FilmDaoSQLImpl implements FilmDao {
 
     @Override
     public Film add(Film item) {
-        String insert = "INSERT INTO film(ime) VALUES(?)";
+        String insert = "INSERT INTO film(ime, zanr, trajanje) VALUES(?, ?, ?)";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, item.getIme());
+            stmt.setObject(2, item.getZanr());
+            stmt.setObject(3, item.getTrajanje());
             stmt.executeUpdate();
-
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next(); // we know that there is one key
             item.setId(rs.getInt(1)); //set id to return it back
@@ -61,11 +62,13 @@ public class FilmDaoSQLImpl implements FilmDao {
 
     @Override
     public Film update(Film item) {
-        String insert = "UPDATE film SET ime = ? WHERE id = ?";
+        String insert = "UPDATE film SET ime = ?, zanr = ?, trajanje = ? WHERE id = ?";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
             stmt.setObject(1, item.getIme());
-            stmt.setObject(2, item.getId());
+            stmt.setObject(2, item.getZanr());
+            stmt.setObject(3, item.getTrajanje());
+            stmt.setObject(4, item.getId());
             stmt.executeUpdate();
             return item;
         } catch (SQLException e) {
@@ -92,6 +95,54 @@ public class FilmDaoSQLImpl implements FilmDao {
         List<Film> filmovi = new ArrayList<Film>();
         try {
             PreparedStatement stmt = this.connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) { // result set is iterator.
+                Film film = new Film();
+                film.setId(rs.getInt("id"));
+                film.setIme(rs.getString("ime"));
+                film.setZanr(rs.getString("zanr"));
+                film.setTrajanje(rs.getInt("trajanje"));
+                filmovi.add(film);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace(); // poor error handling
+        }
+        return filmovi;
+    }
+
+    @Override
+    public List<Film> searchByTrajanje(int a, int b) {
+        if(a > b) throw new IllegalArgumentException("Nevalidan unos!");
+        String query = "SELECT * FROM film WHERE trajanje BETWEEN ? AND ?";
+        List<Film> filmovi = new ArrayList<Film>();
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setObject(1, a);
+            stmt.setObject(2, b);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) { // result set is iterator.
+                Film film = new Film();
+                film.setId(rs.getInt("id"));
+                film.setIme(rs.getString("ime"));
+                film.setZanr(rs.getString("zanr"));
+                film.setTrajanje(rs.getInt("trajanje"));
+                filmovi.add(film);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace(); // poor error handling
+        }
+        return filmovi;
+    }
+
+    @Override
+    public List<Film> searchByZanr(String z) {
+        String query = "SELECT * FROM film WHERE zanr = ?";
+        List<Film> filmovi = new ArrayList<Film>();
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setObject(1, z);
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) { // result set is iterator.
                 Film film = new Film();
