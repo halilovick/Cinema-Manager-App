cf9bece77c89a9aa6af68397b0d6eb1d18dfe037
halilovick <khalilovic02@gmail.com>
2022-12-15 15:41:57 +0100

    implemented zatvoriButtonClick

--- src/main/java/ba/unsa/etf/rpr/Controllers/KupljenaKartaController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/KupljenaKartaController.java
@@ -1,9 +1,20 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class KupljenaKartaController {
     public Label imeFilma;
     public Label datumFilma;
     public Label cijenaKarte;
+    public Button zatvoriButton;
+
+    public void zatvoriButtonClick(ActionEvent actionEvent) {
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
+    }
 }
