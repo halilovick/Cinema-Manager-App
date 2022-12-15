05d34708b25ce679f4e66dfe80e243a5d91bfaa5
halilovick <khalilovic02@gmail.com>
2022-12-15 13:08:18 +0100

    implemented KupljenaKarta controller and scene

---
+++ src/main/java/ba/unsa/etf/rpr/Controllers/KupljenaKartaController.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.Controllers;
+
+import javafx.scene.control.Label;
+
+public class KupljenaKartaController {
+    public Label imeFilma;
+    public Label datumFilma;
+    public Label cijenaKarte;
+}
