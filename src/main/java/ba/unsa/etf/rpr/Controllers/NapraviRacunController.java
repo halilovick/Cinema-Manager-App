532184eff959d0bf87c19b307dcc5d989bf3bcad
halilovick <khalilovic02@gmail.com>
2022-12-26 12:09:51 +0100

    added email validator to NapraviRacun

--- src/main/java/ba/unsa/etf/rpr/Controllers/NapraviRacunController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/NapraviRacunController.java
@@ -3,12 +3,16 @@ package ba.unsa.etf.rpr.Controllers;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.FilmoviException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import java.sql.Date;
 
@@ -20,8 +24,26 @@ public class NapraviRacunController {
     public TextField gradTextField;
     public DatePicker datumRodjenjaField;
     public TextField imeTextField;
+
+    @FXML
+    void initialize() {
+        emailTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                EmailValidator validator = EmailValidator.getInstance();
+                if (validator.isValid(newValue)) {
+                    emailTextField.getStyleClass().removeAll("poljeNijeIspravno");
+                    emailTextField.getStyleClass().add("poljeIspravno");
+                } else {
+                    emailTextField.getStyleClass().removeAll("poljeIspravno");
+                    emailTextField.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
     public void napraviAccountButtonClick(ActionEvent actionEvent) throws FilmoviException {
-        if(usernameTextField.getText().isEmpty() || lozinkaTextField.getText().isEmpty() || imeTextField.getText().isEmpty() || emailTextField.getText().isEmpty() || adresaTextField.getText().isEmpty() || gradTextField.getText().isEmpty() || datumRodjenjaField.getValue() == null){
+        if (usernameTextField.getText().isEmpty() || lozinkaTextField.getText().isEmpty() || imeTextField.getText().isEmpty() || emailTextField.getText().isEmpty() || adresaTextField.getText().isEmpty() || gradTextField.getText().isEmpty() || datumRodjenjaField.getValue() == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("Nedostaju podaci!");
