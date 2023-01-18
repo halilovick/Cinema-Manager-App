a524e14984169c976d5bcf0daa3b9ff3f63cb471
halilovick <khalilovic02@gmail.com>
2023-01-18 23:50:11 +0100

    implemented delete film CLI method

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -9,7 +9,8 @@ import org.apache.commons.cli.*;
 import java.io.PrintWriter;
 
 public class App {
-    private static final Option addFilm = new Option("f", "add-film", false, "Adding new film to database");
+    private static final Option addFilm = new Option("f", "add-film", false, "Adding new film to database (name, genre, duration)");
+    private static final Option deleteFilm = new Option("delF", "delete-film", false, "Deleting a film from database (name)");
     private static final Option getFilms = new Option("getF", "get-films", false, "Printing all films from database");
     private static final Option getUsers = new Option("getU", "get-users", false, "Printing all users from database");
 
@@ -24,6 +25,7 @@ public class App {
     public static Options addOptions() {
         Options options = new Options();
         options.addOption(addFilm);
+        options.addOption(deleteFilm);
         options.addOption(getFilms);
         options.addOption(getUsers);
         return options;
@@ -44,6 +46,18 @@ public class App {
             f.setCijena(Integer.parseInt(cl.getArgList().get(2)));
             fm.add(f);
             System.out.println("Film successfully added to database!");
+        } else if (cl.hasOption(deleteFilm.getOpt()) || cl.hasOption(deleteFilm.getLongOpt())) {
+            filmoviManager fm = new filmoviManager();
+            Film f = new Film();
+            try {
+                f = fm.getByIme(cl.getArgList().get(0));
+                fm.delete(f.getId());
+                System.out.println("Film successfuly deleted from database!");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Film with that name does not exist.");
+            } catch (FilmoviException e) {
+                System.out.println("Film cannot be deleted. First delete related tickets before deleting film.");
+            }
         } else if (cl.hasOption(getFilms.getOpt()) || cl.hasOption(getFilms.getLongOpt())) {
             filmoviManager fm = new filmoviManager();
             fm.getAll().forEach(f -> System.out.println(f.getIme()));
