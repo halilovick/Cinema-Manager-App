ee6dce2dc7858bb9e73bc1cbc7a7ad147306919a
halilovick <khalilovic02@gmail.com>
2023-01-19 00:24:51 +0100

    added delete tickets CLI method

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.business.filmoviManager;
+import ba.unsa.etf.rpr.business.karteManager;
 import ba.unsa.etf.rpr.business.usersManager;
 import ba.unsa.etf.rpr.domain.Film;
 import ba.unsa.etf.rpr.exceptions.FilmoviException;
@@ -12,7 +13,7 @@ public class App {
     private static final Option addFilm = new Option("f", "add-film", false, "Adding new film to database (name, genre, duration)");
     private static final Option deleteFilm = new Option("delF", "delete-film", false, "Deleting a film from database (\"name\")");
     private static final Option getFilms = new Option("getF", "get-films", false, "Printing all films from database");
-    private static final Option deleteTickets = new Option("delT", "delete-tickets", false, "Deleting existing tickets (ticket id to delete single ticket or name of film to delete all related tickets)");
+    private static final Option deleteTickets = new Option("delT", "delete-tickets", false, "Deleting existing tickets (ticket id to delete single ticket or \"name\" of film to delete all related tickets)");
     private static final Option getUsers = new Option("getU", "get-users", false, "Printing all users from database");
 
     public static void printFormattedOptions(Options options) {
@@ -31,6 +32,15 @@ public class App {
         options.addOption(deleteTickets);
         options.addOption(getUsers);
         return options;
+    }
+
+    private static boolean isDigit(String s) {
+        try {
+            int intValue = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public static void main(String[] args) throws ParseException, FilmoviException {
@@ -64,7 +74,23 @@ public class App {
             filmoviManager fm = new filmoviManager();
             fm.getAll().forEach(f -> System.out.println(f.getIme()));
         } else if (cl.hasOption(deleteTickets.getOpt()) || cl.hasOption(deleteTickets.getLongOpt())) {
-
+            karteManager km = new karteManager();
+            filmoviManager fm = new filmoviManager();
+            if (isDigit(cl.getArgList().get(0))) {
+                try {
+                    km.delete(Integer.parseInt(cl.getArgList().get(0)));
+                    System.out.println("Ticket successfully deleted from database!");
+                } catch (FilmoviException f) {
+                    System.out.println("Ticket does not exist!");
+                }
+            } else {
+                try {
+                    km.deleteWithFilmId(fm.getByIme(cl.getArgList().get(0)).getId());
+                    System.out.println("Tickets successfully deleted from database!");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Film with that name does not exist.");
+                }
+            }
         } else if (cl.hasOption(getUsers.getOpt()) || cl.hasOption(getUsers.getLongOpt())) {
             usersManager um = new usersManager();
             um.getAll().forEach(u -> System.out.println(u.getIme()));
