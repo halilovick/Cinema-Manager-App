ac7ba14b263211b9e8a8cbba863a61883f2ed702
halilovick <khalilovic02@gmail.com>
2023-02-10 21:11:31 +0100

    implemented brisanjeNepostojeceKarte test

---
+++ src/test/java/ba/unsa/etf/rpr/karteManagerTest.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.business.filmoviManager;
+import ba.unsa.etf.rpr.business.karteManager;
+import ba.unsa.etf.rpr.business.usersManager;
+import ba.unsa.etf.rpr.exceptions.FilmoviException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class karteManagerTest {
+
+    filmoviManager fm = new filmoviManager();
+    karteManager km = new karteManager();
+    usersManager um = new usersManager();
+
+    @Test
+    void brisanjeNepostojeceKarte() {
+        Assertions.assertThrows(FilmoviException.class, () -> {
+            km.delete(1000);
+        });
+    }
+}
