05f8f5f354d107c3394fd7e35c589190a133b71b
halilovick <khalilovic02@gmail.com>
2023-02-11 13:26:42 +0100

    javadoc for manager classes

--- src/main/java/ba/unsa/etf/rpr/business/karteManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/karteManager.java
@@ -6,6 +6,9 @@ import ba.unsa.etf.rpr.exceptions.FilmoviException;
 
 import java.util.List;
 
+/**
+ * Manager class for karte
+ */
 public class karteManager {
     private boolean kartaExists(int id) throws FilmoviException {
         karteManager km = new karteManager();
@@ -17,6 +20,12 @@ public class karteManager {
         return true;
     }
 
+    /**
+     * Adds ticket to database
+     *
+     * @param k the ticket
+     * @throws FilmoviException the filmovi exception
+     */
     public Karta add(Karta k) throws FilmoviException {
         try {
             return DaoFactory.kartaDao().add(k);
@@ -25,6 +34,12 @@ public class karteManager {
         }
     }
 
+    /**
+     * Deletes ticket from database
+     *
+     * @param kartaId the ticket id
+     * @throws FilmoviException the filmovi exception
+     */
     public void delete(int kartaId) throws FilmoviException {
         if (!kartaExists(kartaId)) throw new FilmoviException("Karta does not exist!");
         try {
@@ -34,26 +49,53 @@ public class karteManager {
         }
     }
 
+    /**
+     * Updates tickets in database.
+     *
+     * @param karta the ticket
+     * @throws FilmoviException the filmovi exception
+     */
     public Karta update(Karta karta) throws FilmoviException {
         return DaoFactory.kartaDao().update(karta);
     }
 
+    /**
+     * Gets all tickets from database.
+     *
+     * @return a list with tickets.
+     * @throws FilmoviException the filmovi exception
+     */
     public List<Karta> getAll() throws FilmoviException {
         return DaoFactory.kartaDao().getAll();
     }
 
+    /**
+     * Gets ticket by id.
+     *
+     * @param id the ticket id
+     * @return the ticket
+     * @throws FilmoviException the filmovi exception
+     */
     public Karta getById(int id) throws FilmoviException {
         return DaoFactory.kartaDao().getById(id);
     }
 
-    public void deleteAll() throws FilmoviException {
-        DaoFactory.kartaDao().deleteAll();
-    }
-
+    /**
+     * Deletes tickets with related film id.
+     *
+     * @param id the film id
+     * @throws FilmoviException the filmovi exception
+     */
     public void deleteWithFilmId(int id) throws FilmoviException {
         DaoFactory.kartaDao().deleteWithFilmId(id);
     }
 
+    /**
+     * Deletes tickets with related user id.
+     *
+     * @param id the user id
+     * @throws FilmoviException the filmovi exception
+     */
     public void deleteWithUserId(int id) throws FilmoviException {
         DaoFactory.kartaDao().deleteWithUserId(id);
     }
--- src/main/java/ba/unsa/etf/rpr/business/usersManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/usersManager.java
@@ -6,6 +6,9 @@ import ba.unsa.etf.rpr.exceptions.FilmoviException;
 
 import java.util.List;
 
+/**
+ * Manager class for users.
+ */
 public class usersManager {
     private boolean userExists(int id) throws FilmoviException {
         usersManager um = new usersManager();
@@ -17,6 +20,12 @@ public class usersManager {
         return true;
     }
 
+    /**
+     * Adds user to database.
+     *
+     * @param u the user
+     * @throws FilmoviException the filmovi exception
+     */
     public User add(User u) throws FilmoviException {
         if (userExists(u.getUser())) throw new FilmoviException("User with such username already exists!");
         try {
@@ -26,6 +35,12 @@ public class usersManager {
         }
     }
 
+    /**
+     * Deletes user from database.
+     *
+     * @param userId the user id
+     * @throws FilmoviException the filmovi exception
+     */
     public void delete(int userId) throws FilmoviException {
         if (!userExists(userId)) throw new FilmoviException("User does not exist!");
         try {
@@ -35,26 +50,67 @@ public class usersManager {
         }
     }
 
+    /**
+     * Updates user in database.
+     *
+     * @param u the user
+     * @throws FilmoviException the filmovi exception
+     */
     public User update(User u) throws FilmoviException {
         return DaoFactory.usersDao().update(u);
     }
 
+    /**
+     * Gets all users from database.
+     *
+     * @return list with all the users.
+     * @throws FilmoviException the filmovi exception
+     */
     public List<User> getAll() throws FilmoviException {
         return DaoFactory.usersDao().getAll();
     }
 
+    /**
+     * Gets id from logged in user.
+     *
+     * @param username the username
+     * @param password the password
+     * @return the id
+     * @throws FilmoviException the filmovi exception
+     */
     public int getLoggedInId(String username, String password) throws FilmoviException {
         return DaoFactory.usersDao().getLoggedInId(username, password);
     }
 
+    /**
+     * Gets user by id.
+     *
+     * @param id the user id
+     * @return the user by id
+     * @throws FilmoviException the filmovi exception
+     */
     public User getById(int id) throws FilmoviException {
         return DaoFactory.usersDao().getById(id);
     }
 
+    /**
+     * Checks if user exists
+     *
+     * @param username the username
+     * @return the boolean
+     * @throws FilmoviException the filmovi exception
+     */
     public boolean userExists(String username) throws FilmoviException {
         return DaoFactory.usersDao().userExists(username);
     }
 
+    /**
+     * Gets user by username.
+     *
+     * @param username the username
+     * @return the user by username
+     * @throws FilmoviException the filmovi exception
+     */
     public User getByUsername(String username) throws FilmoviException {
         return DaoFactory.usersDao().getByUsername(username);
     }
